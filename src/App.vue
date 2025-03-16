be1ab4c7248eb3ab60215c00f4b9dc4241a323dc
Vaness <vaness13337@gmail.com>
2025-03-16 19:53:35 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import Nav from './components/NavBar.vue'
+</script>
+
+<template>
+  <main class="font-main">
+    <Nav />
+  </main>
+
+</template>
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900 w-full bg-yellow-main-1">
+    <div class="gap-20 flex flex-wrap items-center justify-center mx-auto py-2 w-full">
+      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="./icons/LogoNavbar.png" class="h-16" alt="Flowbite Logo" />
+      </a>
+      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex gap-4 items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2" aria-controls="navbar-default" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+      </button>
+      <div class="hidden w-full md:block md:w-auto text-white" id="navbar-default">
+        <ul class="font-medium gap-4 flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+            <li class="bg-yellow-main-2 px-4 py-2 rounded-sm">
+              <a href="#" class="block py-2 px-3 rounded-sm md:bg-transparent md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+            </li>
+            <li class="px-4 py-2 rounded-sm hover:bg-yellow-main-2">
+              <a href="#" class="block py-2 px-3 rounded-sm md:hover:bg-transparent md:border-0 md:p-0">About</a>
+            </li>
+            <li class="px-4 py-2 rounded-sm hover:bg-yellow-main-2">
+              <a href="#" class="block py-2 px-3 rounded-sm md:hover:bg-transparent md:border-0 md:p-0">Services</a>
+            </li>
+            <li class="px-4 py-2 rounded-sm hover:bg-yellow-main-2">
+              <a href="#" class="block py-2 px-3 rounded-sm md:hover:bg-transparent md:border-0 md:p-0">Pricing</a>
+            </li>
+            <li class="px-4 py-2 rounded-sm hover:bg-yellow-main-2">
+              <a href="#" class="block py-2 px-3 rounded-sm md:hover:bg-transparent md:border-0 md:p-0">Contact</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
